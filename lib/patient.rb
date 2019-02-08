a65630d6472ae0b557b3dd63c06c2daa671d2168
Alexandra Palermo <alexmpalermo@gmail.com>
2019-02-08 17:37:16 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -14,7 +14,16 @@ class Patient
   end
   
   def new_appointment(doctor, date)
-  
+   appt = Appointment.new(date, self, doctor)
+ end 
+ 
+  def appointments
+  Appointment.all.select {|x| x.patient == self}
+ end
+ 
+  def doctors
+    appointments.collect {|x| x.doctor}
+  end 
   
   
   
