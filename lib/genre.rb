49fb19236b02e074564b238a096661b936997b6f
Alexandra Palermo <alexmpalermo@gmail.com>
2019-02-08 17:24:25 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,11 +1,10 @@
 class Genre 
-  attr_accessor :name, :songs
+  attr_accessor :name
   
   @@all = []
   
   def initialize(name)
     @name = name
-    @songs = []
     @@all << self 
   end
   
@@ -19,7 +18,7 @@ class Genre
   end 
   
   def artists
-    
+    self.songs.collect {|x| x.artist}
   end
   
 end 
