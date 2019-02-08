23aed196c63e32c2af9fa034fa79f986ea534f62
Alexandra Palermo <alexmpalermo@gmail.com>
2019-02-08 17:09:23 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,7 +16,7 @@ class Artist
  def new_song(name, genre)
    song = Song.new(name, self, genre)
    song.artist = self 
-    @songs << song unless @songs.include?(song)
+    @songs << song 
      
   end 
   
