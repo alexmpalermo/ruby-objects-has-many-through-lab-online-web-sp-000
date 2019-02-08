9554b65259005af8ba4a3bb00e65cf30ae4f2146
Alexandra Palermo <alexmpalermo@gmail.com>
2019-02-08 17:31:34 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,31 @@
+class Doctor
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def new_appointment(patient, date)
+    appt = Appointment.new(patient, self, date)
+  end 
+  
+  def appointments
+    Appointment.all.select {|x| x.doctor == self}
+  end 
+  
+  def patients
+    appointments.collect {|x| x.patient}
+  end 
+  
+  
+  
+  
+  
+end
